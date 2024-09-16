3b23417bbc3534914013d5e769358d48ab1df154
Nabeel Afzal <nabeel.afzal@cartlow.com>
2024-09-16 12:19:04 +0500

    Add version

--- src/Resources/views/layouts/master.blade.php
+++ src/Resources/views/layouts/master.blade.php
@@ -9,13 +9,13 @@
         <title>{{ env('APP_NAME') }} | Make transaction</title>
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
        {{-- Laravel Mix - CSS File --}}
-       <link rel="stylesheet" href="{{ localAsset('css/coinpayment.css') }}">
+       <link rel="stylesheet" href="{{ localAsset('css/coinpayment.css').'?_='.config('app.version') }}">
     </head>
     <body style="font-family:{{ config('coinpayment.font.family') }};">
         <div class="container">
             @yield('content')
         </div>
         {{-- Laravel Mix - JS File --}}
-        <script src="{{ localAsset('js/coinpayment.js') }}"></script>
+        <script src="{{ localAsset('js/coinpayment.js').'?_='.config('app.version') }}"></script>
     </body>
 </html>
